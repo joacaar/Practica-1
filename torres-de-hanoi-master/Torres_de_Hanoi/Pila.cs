6286ff2d18965e4b5fba9e3022de048f27cda157
Joan Calabuig <joacaar@epsg.upv.es>
2019-02-18 22:19:18 +0100

    Clase Pila
    Se ha implementado toda la clase Pila y se ha testeado casi todos los posibles casos

--- torres-de-hanoi-master/Torres_de_Hanoi/Pila.cs
+++ torres-de-hanoi-master/Torres_de_Hanoi/Pila.cs
@@ -43,19 +43,84 @@ namespace Torres_de_Hanoi
             
         }
 
+        // Añadir un disco a la pila, Si el disco es de menor tamaño que el que se encuentra en la pila,
+        //se podra poner, si la pila esta vacia se podra poner, si el disco que se encuentra es menor que el que se va aintroducir no hara nada
         public void push(Disco d)
         {
+            Console.WriteLine("Inicio del Push");
+
+            if (isEmpty())
+            {
+                Elementos.Insert(0, d);
+                Size = Elementos.Count;
+                Top = Elementos[Size-1].Valor;
+                
+            }
+            else if (Elementos[0].Valor > (d.Valor))
+            {
+                Elementos.Insert(0, d);
+                Size = Elementos.Count;
+                Top = Elementos[Size - 1].Valor;
+
+            }
+            else
+            {
+                Console.WriteLine("Movimiento no válido");
+                return;
+            }
+
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine("Elemento en: " + i + " : " + Elementos[i].Valor);
+            }
+
+            Console.WriteLine("Fin del Push");
+
+            Console.WriteLine("---------------------------------------------------");
+
 
         }
 
         public Disco pop()
         {
+            Console.WriteLine("En la funcion POP");
+
+            if (!isEmpty())
+            {
+                Console.WriteLine("Elementos iniciales");
+
+                for (int i = 0; i < Size; i++)
+                {
+                    Console.WriteLine("Elemento en: " + i + " : " + Elementos[i].Valor);
+                }
+                Disco aux = new Disco();
+                aux.Valor = Elementos[0].Valor;
+
+                Elementos.RemoveAt(0);
+                Size = Elementos.Count;
+                Top = Elementos[0].Valor;
+                Console.WriteLine("Elementos finales");
+
+                for (int i = 0; i < Size; i++)
+                {
+                    Console.WriteLine("Elemento en: " + i + " : " + Elementos[i].Valor);
+                }
+                Console.WriteLine("Fin de pop");
+                return aux;
+            }
+
+            Console.WriteLine("No hay elementos");
+
             return null;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if (Elementos.Count.Equals(0))
+            {
+                return true;
+            }
+            return false;
         }
 
     }
--- torres-de-hanoi-master/Torres_de_Hanoi/Program.cs
+++ torres-de-hanoi-master/Torres_de_Hanoi/Program.cs
@@ -10,8 +10,28 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
-
+            Pila Aux = new Pila();
+            Pila Fin = new Pila();
             Pila Inicio = new Pila(3);
+
+            Console.WriteLine(Aux.Size);
+
+            Disco d = new Disco();
+            d.Valor = 4;
+            
+            Disco d2 = new Disco();
+            d2.Valor = 3;
+
+            Disco d3 = new Disco();
+            d3.Valor = 5;
+
+            Aux.push(d);
+            Aux.push(d2);
+            Aux.push(d3);
+            Disco d4 = Aux.pop();
+
+            Fin.push(d4);
+
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key exit.");
