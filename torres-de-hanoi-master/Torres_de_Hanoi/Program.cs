3233e5da71fce6b01277bfab47104cbb4a086fe3
Joan Calabuig <joacaar@epsg.upv.es>
2019-02-21 00:19:54 +0100

    Programa funciona
    Testeado con varios discos y funciona con todos.

--- torres-de-hanoi-master/Torres_de_Hanoi/Program.cs
+++ torres-de-hanoi-master/Torres_de_Hanoi/Program.cs
@@ -10,16 +10,37 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+
+            int numDiscos = 10;
             Hanoi hanoi = new Hanoi();
 
             Pila Aux = new Pila();
             Aux.nombre = "Auxiliar";
             Pila Fin = new Pila();
             Fin.nombre = "Fin";
-            Pila Inicio = new Pila(3);
+            Pila Inicio = new Pila(numDiscos);
             Inicio.nombre = "Inicio";
 
+            Console.WriteLine("Inicio de partida --------------------------------");
 
+            hanoi.mostrarMovimiento(Inicio);
+            hanoi.mostrarMovimiento(Aux);
+            hanoi.mostrarMovimiento(Fin);
+
+
+            Console.WriteLine("---------------------------------------------------");
+
+            //hanoi.iterativo(3,Inicio, Fin, Aux);
+
+            Console.WriteLine("Fin de partida --------------------------------");
+
+            int mov = hanoi.iterativo(numDiscos, Inicio, Fin, Aux);
+
+            hanoi.mostrarMovimiento(Inicio);
+            hanoi.mostrarMovimiento(Aux);
+            hanoi.mostrarMovimiento(Fin);
+            Console.WriteLine("Total movimientos : " + mov);
+            Console.WriteLine("------------------------------------------------");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key exit.");
